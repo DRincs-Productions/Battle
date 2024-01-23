9cfaccd658828af46c1e0ac0151ffcd4aa49f5e4
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-01-23 11:27:51 +0100

    Add button enable/disable functionality to PlayerStatistics class

--- pythonpackages/boxing_battle/character_statistics.py
+++ pythonpackages/boxing_battle/character_statistics.py
@@ -127,6 +127,15 @@ class PlayerStatistics(FightingStatistics):
         self.left_button = left_button
         self.right_button = right_button
 
+        self.x_enabled = True
+        self.y_enabled = True
+        self.a_enabled = True
+        self.b_enabled = True
+        self.up_enabled = True
+        self.down_enabled = True
+        self.left_enabled = True
+        self.right_enabled = True
+
     @property
     def x_button(self) -> Optional[FightingMove]:
         """The x button move."""
@@ -198,3 +207,97 @@ class PlayerStatistics(FightingStatistics):
     @right_button.setter
     def right_button(self, value: Optional[FightingMove]):
         self._right_button = value
+
+    @property
+    def x_enabled(self) -> bool:
+        """If the x button is enabled."""
+        return self._x_enabled
+
+    @x_enabled.setter
+    def x_enabled(self, value: bool):
+        self._x_enabled = value
+
+    @property
+    def y_enabled(self) -> bool:
+        """If the y button is enabled."""
+        return self._y_enabled
+
+    @y_enabled.setter
+    def y_enabled(self, value: bool):
+        self._y_enabled = value
+
+    @property
+    def a_enabled(self) -> bool:
+        """If the a button is enabled."""
+        return self._a_enabled
+
+    @a_enabled.setter
+    def a_enabled(self, value: bool):
+        self._a_enabled = value
+
+    @property
+    def b_enabled(self) -> bool:
+        """If the b button is enabled."""
+        return self._b_enabled
+
+    @b_enabled.setter
+    def b_enabled(self, value: bool):
+        self._b_enabled = value
+
+    @property
+    def up_enabled(self) -> bool:
+        """If the up button is enabled."""
+        return self._up_enabled
+
+    @up_enabled.setter
+    def up_enabled(self, value: bool):
+        self._up_enabled = value
+
+    @property
+    def down_enabled(self) -> bool:
+        """If the down button is enabled."""
+        return self._down_enabled
+
+    @down_enabled.setter
+    def down_enabled(self, value: bool):
+        self._down_enabled = value
+
+    @property
+    def left_enabled(self) -> bool:
+        """If the left button is enabled."""
+        return self._left_enabled
+
+    @left_enabled.setter
+    def left_enabled(self, value: bool):
+        self._left_enabled = value
+
+    @property
+    def right_enabled(self) -> bool:
+        """If the right button is enabled."""
+        return self._right_enabled
+
+    @right_enabled.setter
+    def right_enabled(self, value: bool):
+        self._right_enabled = value
+
+    def disable_all_buttons(self):
+        """Disable all buttons."""
+        self.x_enabled = False
+        self.y_enabled = False
+        self.a_enabled = False
+        self.b_enabled = False
+        self.up_enabled = False
+        self.down_enabled = False
+        self.left_enabled = False
+        self.right_enabled = False
+
+    def enable_all_buttons(self):
+        """Enable all buttons."""
+        self.x_enabled = True
+        self.y_enabled = True
+        self.a_enabled = True
+        self.b_enabled = True
+        self.up_enabled = True
+        self.down_enabled = True
+        self.left_enabled = True
+        self.right_enabled = True
