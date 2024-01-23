32679536dc86168efbe0430733ba460c9e38fadd
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-01-23 10:28:50 +0100

    Refactor character statistics class to include stamina and recovery percentage

--- pythonpackages/boxing_battle/character_statistics.py
+++ pythonpackages/boxing_battle/character_statistics.py
@@ -5,10 +5,14 @@ class FightingStatistics:
     def __init__(
         self,
         health: int,
-        strength: int,
+        stamina: int,
+        recovery_percentage_stamina: float,
     ):
         self.health = health
-        self.strength = strength
+        self.stamina = stamina
+        self.max_health = health
+        self.max_stamina = stamina
+        self.recovery_percentage_stamina = recovery_percentage_stamina
 
     @property
     def health(self) -> int:
@@ -20,13 +24,40 @@ class FightingStatistics:
         self._health = value
 
     @property
-    def strength(self) -> int:
-        """The strength of the character."""
-        return self._strength
+    def max_health(self) -> int:
+        """The max health of the character."""
+        return self._max_health
 
-    @strength.setter
-    def strength(self, value: int):
-        self._strength = value
+    @max_health.setter
+    def max_health(self, value: int):
+        self._max_health = value
+
+    @property
+    def stamina(self) -> int:
+        """The stamina of the character."""
+        return self._stamina
+
+    @stamina.setter
+    def stamina(self, value: int):
+        self._stamina = value
+
+    @property
+    def max_stamina(self) -> int:
+        """The max stamina of the character."""
+        return self._max_stamina
+
+    @max_stamina.setter
+    def max_stamina(self, value: int):
+        self._max_stamina = value
+
+    @property
+    def recovery_percentage_stamina(self) -> float:
+        """The recovery percentage of the stamina."""
+        return self._recovery_percentage_stamina
+
+    @recovery_percentage_stamina.setter
+    def recovery_percentage_stamina(self, value: float):
+        self._recovery_percentage_stamina = value
 
     def dannage(
         self,
@@ -42,4 +73,21 @@ class FightingStatistics:
             health_damage = 0
 
         self.health -= health_damage
-        self.strength -= stamina_damage
+        self.stamina -= stamina_damage
+
+    @property
+    def is_dead(self) -> bool:
+        """If the character is dead."""
+        return self.health <= 0
+
+    @property
+    def is_stamina_empty(self) -> bool:
+        """If the stamina is empty."""
+        return self.stamina <= 0
+
+    def recover_stamina(self):
+        """Recover the stamina."""
+        amt = self.stamina * self.recovery_percentage_stamina / 100
+        self.stamina += int(amt)
+        if self.stamina > self.max_stamina:
+            self.stamina = self.max_stamina
