36aa0c9c9cbe3e848c331e4e20c90ef46eab4b14
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-01-27 11:14:19 +0100

    Add opponent attack animation and update move property

--- game/images.rpy
+++ game/images.rpy
@@ -3,4 +3,7 @@ image icon punch = "/boxing_interface/attack-0.webp"
 image opponent block = "defense_b.webp"
 image opponent idle = "idle_b.webp"
 image opponent damage = "dodge_b.webp"
-image opponent attack = "attack_b.webp"
+image opponent attack:
+    "attack_b.webp"
+    pause 0.2
+    "idle_b.webp"
--- pythonpackages/boxing_battle/character_statistics.py
+++ pythonpackages/boxing_battle/character_statistics.py
@@ -579,6 +579,7 @@ class OpponentStatistics(FightingStatistics):
                 return self.idle_image
         return self.current_move.animation_image
 
+    @property
     def update_move(self) -> Optional[FightingMove]:
         """Return the move of the opponent."""
         if self.current_state == FightingState.DAMAGED:
